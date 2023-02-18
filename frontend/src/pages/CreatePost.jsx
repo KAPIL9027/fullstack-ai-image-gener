6fef0e6cfa20d3d199acc662e17f0369499ebfc5
Kapil <sharmaakapil01@gmail.com>
2023-02-18 14:30:36 +0530

    Completed creating the post and adding it to the database

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -13,9 +13,39 @@ function CreatePost() {
   });
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
-  const handleSubmit = ()=>
+  const handleSubmit = async (e)=>
 {
+    e.preventDefault();
+    if(form.prompt && form.photo)
+    {
+      setLoading(true);
+      try
+      {
+        const response = await fetch('http://localhost:8080/api/v1/post',
+        {
+          method:'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(form)
+        });
 
+        await response.json();
+        navigate('/');
+      }
+      catch(error)
+      {
+        alert(error);
+      }
+      finally
+      {
+        setLoading(false);
+      }
+    }
+    else
+    {
+       alert('Please provide a prompt and a photo')
+    }
 }
   const handleChange = (e)=>
   {
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,7 +16,41 @@ const RenderCards = ({data,title})=>
 function Home() {
    const [loading,setLoading] = useState(false);
    const [allPosts,setAllPosts] = useState(null);
-   const [searchText,setSearchText] = useState('')
+   const [searchText,setSearchText] = useState('');
+
+   useEffect(()=>
+   {
+    const fetchPosts = async ()=>
+    {
+      setLoading(true);
+
+      try
+      {
+        const response = await fetch('http://localhost:8080/api/v1/post',
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+
+        if(response.ok)
+        {
+          const result = await response.json();
+          setAllPosts(result.data.reverse());
+        }
+      }
+      catch(error)
+      {
+        alert(error)
+      }
+      finally
+      {
+        setLoading(false)
+      }
+    }
+    fetchPosts();
+   },[])
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
@@ -62,7 +96,7 @@ function Home() {
                    title="No result found"
                    /> 
                    :
-                   <RenderCards data={[]}
+                   <RenderCards data={allPosts}
                     title="No posts found"/>
 
                 }
