486b922d7744a71286d87e854d6009e98c8b3269
Kapil <sharmaakapil01@gmail.com>
2023-05-31 11:32:09 +0530

    deployed backend on render.com and changed the url on frontend

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -21,7 +21,7 @@ function CreatePost() {
       setLoading(true);
       try
       {
-        const response = await fetch('http://localhost:8080/api/v1/post',
+        const response = await fetch('https://dalle2-0-backend.onrender.com/api/v1/post',
         {
           method:'POST',
           headers: {
@@ -64,7 +64,7 @@ function CreatePost() {
       try
       {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle',
+        const response = await fetch('https://dalle2-0-backend.onrender.com/api/v1/dalle',
         {
           method: 'POST',
           headers: {
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -25,7 +25,7 @@ function Home() {
 
       try
       {
-        const response = await fetch('http://localhost:8080/api/v1/post',
+        const response = await fetch('https://dalle2-0-backend.onrender.com/api/v1/post',
         {
           method: 'GET',
           headers: {
